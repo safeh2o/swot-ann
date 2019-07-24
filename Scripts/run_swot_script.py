c16e6912e868cdbfd594ac070f8efd95b93a6e12
Apostolos <toli95@my.yorku.ca>
2019-07-24 12:26:38 -0400

    run_swot_script:
    Uncommented some comments that were there by mistake.

--- Scripts/run_swot_script.py
+++ Scripts/run_swot_script.py
@@ -6,9 +6,6 @@ input_file = sys.argv[1]
 pretrained_network_dir = sys.argv [2]
 results_file = sys.argv[3]
 
-'''The following lines train a network from 
-data written a .csv format. The trained network is saved 
-in a folder called 'Pretrained Network'.'''
 SWOT_net.import_data_from_csv(input_file)
 SWOT_net.import_pretrained_model(pretrained_network_dir)
 SWOT_net.predict()
