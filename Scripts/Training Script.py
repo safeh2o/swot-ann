ad0a6b92ad19b2a694bc297dd04e51c7d8a1d867
Apostolos <toli95@my.yorku.ca>
2019-10-01 22:36:58 -0400

    Modified Training Script to save a pretrained network instance using a second command line argument.

--- Scripts/Training Script.py
+++ Scripts/Training Script.py
@@ -3,9 +3,10 @@ from NNetwork import NNetwork
 
 SWOT_net = NNetwork()
 filename = sys.argv[1]
+output_filename = sys.argv[2]
 
 '''The following lines train a network from 
 data written a .csv format. The trained network is saved 
 in a folder called 'Pretrained Network'.'''
 SWOT_net.import_data_from_csv(filename)
-SWOT_net.train_SWOT_network("Pretrained_Network")
+SWOT_net.train_SWOT_network(output_filename)
