be9351c79ab06f1474b67d46d1b376cb0a2cc6b6
Mohamed Moselhy <look4d@gmail.com>
2022-06-23 07:22:40 -0400

    m1 compatibility

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 reqs = [
-    "tensorflow<=2.8",
+    "tensorflow-macos<=2.8;platform_system=='Darwin'",
+    "tensorflow<=2.8;platform_system!='Darwin'",
     "numpy<=1.20",
     "matplotlib<=3.4.1",
-    "pandas<=1.2.4",
+    "pandas<=1.4.2",
     "scikit-learn<=1.0.2",
     "xlrd<=1.2.0",
     "yattag<=1.14",
@@ -17,7 +18,7 @@ reqs = [
 
 setuptools.setup(
     name="swot-ann-safeh2o",
-    version="2.0.1",
+    version="2.0.2",
     author="SafeH2O",
     author_email="support@safeh2o.app",
     description="SWOT ANN Project",
