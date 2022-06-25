856e012fbbeb7943f6936b98663793cb6bb7b723
Mohamed Moselhy <look4d@gmail.com>
2022-06-25 14:30:34 -0400

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ reqs = [
 
 setuptools.setup(
     name="swot-ann-safeh2o",
-    version="2.0.2",
+    version="2.0.3",
     author="SafeH2O",
     author_email="support@safeh2o.app",
     description="SWOT ANN Project",
