d6e1b340c26c1098a18b85737dd0ee0ae1d68bc1
Mohamed Moselhy <look4d@gmail.com>
2023-10-01 19:40:24 -0400

    upgrade buggy pandas [skip ci]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = [
     "tensorflow==2.9.0;platform_system!='Darwin'",
     "numpy==1.22.0",
     "matplotlib==3.5.2",
-    "pandas==1.4.2",
+    "pandas==1.4.3",
     "scikit-learn==1.0.2",
     "xlrd==1.2.0",
     "yattag==1.14",
@@ -19,7 +19,7 @@ reqs = [
 
 setuptools.setup(
     name="swot-ann-safeh2o",
-    version="2.0.11",
+    version="2.1.0",
     author="SafeH2O",
     author_email="support@safeh2o.app",
     description="SWOT ANN Project",
